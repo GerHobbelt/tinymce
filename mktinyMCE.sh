315eafdfebf9db8cd4d833b7c8eac43e933441dc
Ger Hobbelt <ger@hobbelt.com>
2011-03-23 11:19:44 +0100

    updated build UNIX shell script

--- mktinyMCE.sh
+++ mktinyMCE.sh
@@ -6,17 +6,17 @@ ant build_full
 ant moxiedoc
 ant release
 
-cp tmp/tinymce/tiny_mce.js .
-cp tmp/tinymce/tiny_mce_src.js .
-cp tmp/tinymce/tiny_mce_full.js .
-cp tmp/tinymce/tiny_mce_jquery.js .
-cp tmp/tinymce/tiny_mce_jquery_src.js .
-cp tmp/tinymce/tiny_mce_prototype.js .
-cp tmp/tinymce/tiny_mce_prototype_src.js .
-cp tmp/tinymce/jquery.tinymce.js .
-cp tmp/tinymce/tiny_mce_popup.js .
+cp tmp/tinymce/jscripts/tiny_mce/tiny_mce.js                jscripts/tiny_mce/
+cp tmp/tinymce/jscripts/tiny_mce/tiny_mce_src.js            jscripts/tiny_mce/
+cp tmp/tinymce/jscripts/tiny_mce/tiny_mce_full.js           jscripts/tiny_mce/
+cp tmp/tinymce/jscripts/tiny_mce/tiny_mce_jquery.js         jscripts/tiny_mce/
+cp tmp/tinymce/jscripts/tiny_mce/tiny_mce_jquery_src.js     jscripts/tiny_mce/
+cp tmp/tinymce/jscripts/tiny_mce/tiny_mce_prototype.js      jscripts/tiny_mce/
+cp tmp/tinymce/jscripts/tiny_mce/tiny_mce_prototype_src.js  jscripts/tiny_mce/
+cp tmp/tinymce/jscripts/tiny_mce/jquery.tinymce.js          jscripts/tiny_mce/
+cp tmp/tinymce/jscripts/tiny_mce/tiny_mce_popup.js          jscripts/tiny_mce/
 
-rm -rf tmp/
+# rm -rf tmp/
 
 
 
